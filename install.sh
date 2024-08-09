5f1f91600c416a95d3ce50140b3d9e8fe9b2b49f
catnodes <catnodes@hotmail.com>
2024-08-09 19:06:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing Git..."
-sudo apt update && sudo apt install -y git-all build-essential pkg-config libssl-dev
+sudo apt update && sudo apt install -y git-all build-essential gcc cargo pkg-config libssl-dev
 git --version
 
 echo "Checking if Rust is installed..."
@@ -9,7 +9,7 @@ if ! command -v rustc &> /dev/null; then
     echo "Rust is not installed. Installing Rust..."
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
     echo "Rust installed. Reconfiguring PATH..."
-    source $HOME/.cargo/env
+    . "$HOME/.cargo/env"
 else
     echo "Rust is already installed."
 fi
@@ -24,7 +24,7 @@ fi
 docker --version
 
 echo "Creating new project 'fibonacci'..."
-cargo new fibonacci
+cargo prove new fibonacci
 cd fibonacci || { echo "Failed to change directory to 'fibonacci'"; exit 1; }
 
 echo "Executing Proof..."
@@ -32,12 +32,10 @@ if [ -d "script" ]; then
     cd script || { echo "Failed to change directory to 'script'"; exit 1; }
     
     echo "Running proof execution..."
-    # Pastikan Anda memiliki crate yang mendukung perintah ini
     RUST_LOG=info cargo run --release -- --execute
     echo "Proof execution completed successfully."
     
     echo "Generating Proof..."
-    # Pastikan Anda memiliki crate yang mendukung perintah ini
     RUST_LOG=info cargo run --release -- --prove
     echo "Proof generated and verified successfully."
 else
